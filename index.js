5f28f7f27683b8d4fb4123c9fd60af3225061fee
Maksim Lamianski <iksnaimal@gmail.com>
2021-12-17 12:14:31 +0300

    feat(utils): add utils function

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// UTILS
+function createElement(tag, className, text = ''){
+    const element = document.createElement(tag)
+    element.className = className
+    element.textContent = text
+    return element
+}
